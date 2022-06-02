a05ac862d9346146f5aeb338f09c585505d8de61
Prashant Sawant <prashant@creativegalileo.com>
2022-06-02 14:04:03 +0530

    responsive changes 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import LayeredImage from "react-layered-image";
 import lottieJson from './imgs/1_counting.json';
-import { useEffect, useState } from 'react';
 import Lottie from 'react-lottie-player';
 function App() {
 
@@ -15,9 +12,9 @@ function App() {
       play
       style={{ width: "2000px", height:  "100%" }}
     />
-      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"430px",left:"120px"}}/></a>
-      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"370px",left:"290px"}}/> </a>
-      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"330px",left:"420px"}}/> </a>
+      <a href='https://google.com'> <img src="imgs/logo.png" alt='action bubble 1' style={{height:"60px", position:"absolute", zIndex:"100",top:"430px",left:"120px"}}/></a>
+      <a href='https://google.com'> <img src="imgs/logo.png" alt='action bubble 2' style={{height:"60px", position:"absolute", zIndex:"100",top:"370px",left:"290px"}}/> </a>
+      <a href='https://google.com'> <img src="imgs/logo.png" alt='action bubble 3' style={{height:"60px", position:"absolute", zIndex:"100",top:"330px",left:"420px"}}/> </a>
 
     </div>
   );
