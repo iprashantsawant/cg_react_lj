4730bb1905b8797b5f8aeddb959bf3e8f3303f37
Prashant Sawant <prashant@creativegalileo.com>
2022-06-02 14:00:00 +0530

    responsive changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import LayeredImage from "react-layered-image";
+import lottieJson from './imgs/1_counting.json';
+import { useEffect, useState } from 'react';
+import Lottie from 'react-lottie-player';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Lottie
+      loop
+      animationData={lottieJson}
+      play
+      style={{ width: "2000px", height:  "100%" }}
+    />
+      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"430px",left:"120px"}}/></a>
+      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"370px",left:"290px"}}/> </a>
+      <a href='https://google.com'> <img src="imgs/logo.png" style={{height:"60px", position:"absolute", zIndex:"100",top:"330px",left:"420px"}}/> </a>
+
     </div>
   );
 }
