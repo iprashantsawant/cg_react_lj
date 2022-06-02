36617bc06d14bd7b44284241869ca711e3504701
Prashant Sawant <prashant@creativegalileo.com>
2022-06-02 15:10:24 +0530

    responsive changes 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import Lottie from 'react-lottie-player';
 import { useEffect, useState } from 'react';
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
 function App() {
   const [animationData, setAnimationData] = useState();
+  const [progress, setProgress] = useState(0);
+
   useEffect(() => {
-     import('./imgs/1_counting.json').then(setAnimationData);
+      let p=0;
+      const intc=setInterval(()=>{
+        p=p+10;
+        setProgress(p);
+        if(p==100)
+          clearInterval(intc);
+      },200)
+     import('./imgs/1_counting.json').then((data)=>{
+       setProgress(100);
+       setAnimationData(data);
+     });
   }, []);
-  if (!animationData) return <div style={{"background-image": "linear-gradient(to right, #9bd245 , #84ba39)",height:"100vh"}}>&nbsp;</div>;
+  if (!animationData) 
+  return <div style={{backgroundImage: "linear-gradient(to right, #9bd245 , #84ba39)",height:"100vh"}}>
+            <Progress percent={progress}  style={{width:"80%",marginLeft:"10%",paddingTop:"22%"}} strokeWidth={7}/>
+        </div>;
+  else
   return (
-    <div className="App">
+    <div  style={{backgroundImage: "linear-gradient(to right, #9bd245 , #84ba39)",height:"100%"}}>
       
       <Lottie
       loop
